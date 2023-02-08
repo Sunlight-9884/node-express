a88fa2d2f4cca4e65ce8df7f0a1e7876493f2f0f
Sai Nay LIn Htun <sunlike884@email.com>
2023-02-08 19:38:05 +0630

    updage-node-express

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const fs = express("fs");
+const dotenv = require(".env");
+const event = require("http");
+const path = express("path");
+
 const port = 3000;
 app.set("view engine", "pug"); // testing view engine
 app.use(express.static("public"));
@@ -9,14 +14,14 @@ app.use((req, res, next) => {
 });
 //tesing views close route
 //few user views
-app.get("/profile", (req, res) => {
-  res.render("profile", { title: "hey", message: "Hello there" });
-});
-app.get("/avatar", (req, res) => {
-  res.sendFile(`${__dirname}/photo.png`);
-});
-//const aRoute = require("./routes/a");
-//const userRouter = require(`./routes/user`);
+//app.get("/profile", (req, res) => {
+// res.render("profile", { title: "hey", message: "Hello there" });
+//});
+//app.get("/avatar", (req, res) => {
+// res.sendFile(`${__dirname}/photo.png`);
+//});
+const aRoute = require("./routes/a");
+const userRouter = require(`./routes/user`);
 
 app.use("/a", aRoute);
 app.use("/user", userRouter);
